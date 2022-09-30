81475d32b8b5be7b6812fd92ff41da86642990fe
lfcunha07 <luisfcs3@al.insper.edu.br>
2022-09-30 19:25:13 -0300

    Local arrumada

--- busca-local/busca-local.cpp
+++ busca-local/busca-local.cpp
@@ -20,24 +20,6 @@ double distance(city origin, city dest)
     return sqrt(pow(dest.x - origin.x, 2) + pow(dest.y - origin.y, 2) * 1.0);
 }
 
-// Cria vetor aleatorio
-vector<long> &myFunction(vector<long> &results){
-    for ( auto &i : results) {
-        i*=i;
-    }  
-   return results;
-}
-
-// Printa vetor
-void print_vector(const vector<int> &vect) 
-{
-    long unsigned int i;
-    for (i=0; i<vect.size()-1; i++){
-        cout << vect[i] << " -> "; 
-    }
-    cout << vect[i] << " FIM\n"; 
-}
-
 // MAIN
 int main(int argc, char const *argv[])
 {
@@ -68,56 +50,57 @@ int main(int argc, char const *argv[])
         std::random_device rd; 
         auto rng = std::default_random_engine { rd() };
         std::shuffle(std::begin(city_index), std::end(city_index), rng);
-        for(long unsigned int i=0; i<city_index.size(); i++){
-            best_city_index.at(i) = city_index[i];
-        }
 
         // Calculando distancia inicial
         double initial_distance = 0;
+        std::vector<int> initial_vector (city_index);
         for(long unsigned int i=1; i<city_index.size(); i++){
-            long unsigned int new_origin = city_index[i-1];
-            long unsigned int new_dest = city_index[i];
-            double dist {distance(cidades[new_origin], cidades[new_dest])};
+            double dist {distance(cidades[city_index[i-1]], cidades[city_index[i]])};
             initial_distance += dist;
         }
+        double regress {distance(cidades[city_index[0]], cidades[city_index[city_index.size()-1]])};
+        initial_distance += regress;
+
         if(best_distance == 0 || best_distance > initial_distance){
             best_distance = initial_distance;
+            best_city_index = city_index;
         }
 
         // Realizando Busca Local
-        for (long unsigned int i=0; i<city_index.size()-1; i++){
+        for (long unsigned int i=1; i<city_index.size(); i++){
 
             // Invertendo ordem no vetor
-            std::swap(city_index[i], city_index[i+1]);
+            std::swap(city_index[i-1], city_index[i]);
 
             // Recalculando distancia 
             double round_dist = 0;
             for(long unsigned int i=1; i<city_index.size(); i++){
-                long unsigned int new_origin = city_index[i-1];
-                long unsigned int new_dest = city_index[i];
-                double dist {distance(cidades[new_origin], cidades[new_dest])};
+                double dist {distance(cidades[city_index[i-1]], cidades[city_index[i]])};
                 round_dist += dist;
             }		
             double regress {distance(cidades[city_index[0]], cidades[city_index[city_index.size()-1]])};
 		    round_dist += regress;
 
-            // Checando se a nova distancia e melhor
-            if (round_dist >= best_distance){
-                std::swap(city_index[i+1], city_index[i]);
-            } else {
-                best_distance = round_dist;
-                for(long unsigned int i=0; i<city_index.size(); i++){
-                    best_city_index.at(i) = city_index[i];
-                }
+            // Variável da menor busca local
+            if(round_dist < initial_distance){
+                initial_distance = round_dist;
+                initial_vector = city_index;
             }
-            initial_distance = round_dist;
+
+            // Checando se a nova distancia e melhor
+            if (round_dist < best_distance){
+                best_distance = round_dist;
+                best_city_index = city_index;
+            } else {
+                std::swap(city_index[i-1], city_index[i]);
+            }
         }
 
         // Cout error
         cerr << "local: " << initial_distance << " ";
-        for (size_t i = 0; i < city_index.size(); i++)
+        for (long unsigned int i = 0; i < city_index.size(); i++)
         {
-            cerr << city_index[i] << " ";
+            cerr << initial_vector[i] << " ";
         }
         cerr << "\n";
     }
@@ -128,5 +111,20 @@ int main(int argc, char const *argv[])
         cout << best_city_index[i] << " ";
     }
     cout << "\n";
+
+    // DEBUG
+    double round_dist = 0;
+    for(long unsigned int i=1; i<best_city_index.size(); i++){
+        double dist {distance(cidades[best_city_index[i-1]], cidades[best_city_index[i]])};
+        cout << round_dist << " + " << dist << " = ";
+        round_dist += dist;
+        cout << round_dist << endl;
+    }		
+    double regress {distance(cidades[best_city_index[0]], cidades[best_city_index[best_city_index.size()-1]])};
+    cout << round_dist << " + " << regress << " = ";
+    round_dist += regress;
+    cout << round_dist << endl;
+
+
     return 0;
 }
