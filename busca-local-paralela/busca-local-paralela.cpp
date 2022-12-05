3d52a7f4a0220e13b4b4bee832aafd0652246e9a
lfcunha07 <luisfcs3@al.insper.edu.br>
2022-12-05 20:15:31 -0300

    paralilazao-multi-core

--- busca-local-paralela/busca-local-paralela.cpp
+++ busca-local-paralela/busca-local-paralela.cpp
@@ -5,12 +5,13 @@
 #include <fstream>
 #include <string>
 #include <random>
+#include <omp.h>
 #include <chrono>
+#include <ranges>
 
 using namespace std;
 using namespace std::chrono;
-
-using namespace std;
+using std::begin, std::end;
 
 // Classe cidade
 struct city
@@ -24,34 +25,13 @@ double distance(city origin, city dest)
     return sqrt(pow(dest.x - origin.x, 2) + pow(dest.y - origin.y, 2) * 1.0);
 }
 
-// MAIN
-int main(int argc, char const *argv[])
+// Busca Local
+std::vector<int> local_search(std::vector<int> city_index, long unsigned int n_city)
 {
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    start = std::chrono::system_clock::now();
-    // Pegando o numero de cidades
-    long unsigned int n_city;
-    cin >> n_city;
-    std::vector<int> city_index;
-
-    // Pegando a coordenada das cidades e adicionando ao array
     city cidades[n_city];
-    for(long unsigned int i=0; i<n_city; i++){
-        city cidade;
-        double x,y;
-        cin >> x;
-        cin >> y;
-        cidade.x = x;
-        cidade.y = y;
-        cidades[i] = cidade;
-        city_index.push_back(i);
-    }
-
-    // ------------------------------------------------ BUSCA LOCAL ------------------------------------------------
     double best_distance = 0;
     std::vector<int> best_city_index (city_index);
     for (long unsigned int i=0; i<10*n_city; i++){
-
         // Criando vetor aleatorio
         std::random_device rd; 
         auto rng = std::default_random_engine { rd() };
@@ -70,7 +50,7 @@ int main(int argc, char const *argv[])
         if(best_distance == 0 || best_distance > initial_distance){
             best_distance = initial_distance;
             best_city_index = city_index;
-        }
+        }   
 
         // Realizando Busca Local
         for (long unsigned int i=1; i<city_index.size(); i++){
@@ -85,7 +65,7 @@ int main(int argc, char const *argv[])
                 round_dist += dist;
             }		
             double regress {distance(cidades[city_index[0]], cidades[city_index[city_index.size()-1]])};
-		    round_dist += regress;
+            round_dist += regress;
 
             // Variável da menor busca local
             if(round_dist < initial_distance){
@@ -93,7 +73,7 @@ int main(int argc, char const *argv[])
                 initial_vector = city_index;
             }
 
-            // Checando se a nova distancia e melhor
+            // Checando se a nova distancia é melhor
             if (round_dist < best_distance){
                 best_distance = round_dist;
                 best_city_index = city_index;
@@ -101,7 +81,7 @@ int main(int argc, char const *argv[])
                 std::swap(city_index[i-1], city_index[i]);
             }
         }
-
+    
         // // Cout error
         // cerr << "local: " << initial_distance << " ";
         // for (long unsigned int i = 0; i < city_index.size(); i++)
@@ -110,11 +90,70 @@ int main(int argc, char const *argv[])
         // }
         // cerr << "\n";
     }
+    return city_index;
+}
+
+
+// MAIN
+int main(int argc, char const *argv[])
+{
+    // Tempo
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+
+    // Pegando o numero de cidades
+    long unsigned int n_city;
+    cin >> n_city;
+    std::vector<int> city_index;
+
+    // Pegando a coordenada das cidades e adicionando ao array
+    city cidades[n_city];
+    for(long unsigned int i=0; i<n_city; i++){
+        city cidade;
+        double x,y;
+        cin >> x;
+        cin >> y;
+        cidade.x = x;
+        cidade.y = y;
+        cidades[i] = cidade;
+        city_index.push_back(i);
+    }
+
+    std::vector<std::vector<int>> best_city_index;
+    std::vector<double> best_distances;
+    std::vector<int> city_distance;
+    #pragma omp parallel
+    {
+        city_distance = local_search(city_index, n_city);
+        double best_distance = 0;
+
+        for(long unsigned int i=1; i<city_distance.size(); i++){
+            double dist {distance(cidades[city_distance[i-1]], cidades[city_distance[i]])};
+            best_distance += dist;
+        }
+        double regress {distance(cidades[city_distance[0]], cidades[city_distance[city_distance.size()-1]])};
+        best_distance += regress;
+        #pragma omp critical
+        {
+            best_city_index.push_back(city_distance);
+            best_distances.push_back(best_distance);
+        }
+    }
+
+    double min_dist = best_distances[0];
+    long unsigned int c_val = 0;
+    for(long unsigned int i=1; i<best_distances.size(); i++){
+        if(best_distances[i] < min_dist){
+            min_dist = best_distances[i];
+            c_val = i;
+        }
+    }
+    city_distance = best_city_index[c_val];
 
     // Printa a ordem das cidades
-    cout << best_distance << " 0" << "\n";
-    for(long unsigned int i=0; i<best_city_index.size(); i++){
-        cout << best_city_index[i] << " ";
+    cout << min_dist << " 0" << "\n";
+    for(long unsigned int i=0; i<city_distance.size(); i++){
+        cout << city_distance[i] << " ";
     }
     cout << "\n";
 
@@ -124,7 +163,6 @@ int main(int argc, char const *argv[])
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
   
     std::cout << "finished computation at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << "s\n";
-
 
     return 0;
 }
